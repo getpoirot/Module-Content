4cf6fca8a753fcfd15d0e0a499cd72523719ec47
Payam Naderi <naderi.payam@gmail.com>
2017-03-31 21:46:30 +0430

    implement retrieve content post

---
+++ src/Content/Actions/Posts/RetrievePostAction.php
@@ -0,0 +1,83 @@
+<?php
+namespace Module\Content\Actions\Posts;
+
+use Module\Content;
+use Module\Content\Actions\aAction;
+use Module\Content\Interfaces\Model\Repo\iRepoPosts;
+use Poirot\Application\Exception\exAccessDenied;
+use Poirot\Application\Sapi\Server\Http\ListenerDispatch;
+use Poirot\Http\Interfaces\iHttpRequest;
+use Poirot\OAuth2\Interfaces\Server\Repository\iEntityAccessToken;
+
+
+class RetrievePostAction
+    extends aAction
+{
+    /** @var iRepoPosts */
+    protected $repoPosts;
+
+
+    /**
+     *
+     * @param iHttpRequest $request   @IoC /
+     * @param iRepoPosts   $repoPosts @IoC /module/content/services/repository/Posts
+     */
+    function __construct(iHttpRequest $request, iRepoPosts $repoPosts)
+    {
+        parent::__construct($request);
+
+        $this->repoPosts = $repoPosts;
+    }
+
+
+    /**
+     * Retrieve Post
+     *
+     * - only posts with stat publish and share public will attain here.
+     * - when user is owner of a post the draft and private posts also can be retrieved.
+     *
+     * @param null                    $content_id
+     * @param iEntityAccessToken|null $token
+     *
+     * @return array
+     */
+    function __invoke($content_id = null, iEntityAccessToken $token = null)
+    {
+        # Check Whether Content Post Exists?
+        if( false === $post = $this->repoPosts->findOneByUID($content_id) )
+            throw new Content\Exception\exResourceNotFound(sprintf(
+                'Content Post (%s) Not Found.'
+                , $content_id
+            ));
+
+
+        # Check User Has Permission On Post
+
+        if ($post->getStat() == Content\Model\Entity\EntityPost::STAT_LOCKED)
+            throw new exAccessDenied('Access Denied, Post Is Locked.');
+
+        if (! $this->IsUserPermissionOnContent($post, $token)) {
+            // only posts with stat publish and share public will attain here
+            if (!
+               (  $post->getStat()      == Content\Model\Entity\EntityPost::STAT_PUBLISH
+               || $post->getStatShare() == Content\Model\Entity\EntityPost::STAT_SHARE_PUBLIC )
+            )
+                throw new Content\Exception\exResourceNotFound(sprintf(
+                    'Content Post (%s) Not Found.'
+                    , $content_id
+                ));
+        }
+
+
+        # Build Response
+
+        return [
+            ListenerDispatch::RESULT_DISPATCH =>
+                Content\toArrayResponseFromPostEntity($post) + [
+                    '_self' => [
+                        'content_id' => $content_id,
+                    ],
+                ]
+        ];
+    }
+}
--- src/_functions.php
+++ src/_functions.php
@@ -1,39 +1,73 @@
 <?php
-namespace Module\Content\Lib;
-
-use Module\Content\Services\IOC as ContentIOC;
-use Poirot\Std\Interfaces\Pact\ipFactory;
-
-
-class FactoryContentObject
-    implements ipFactory
+namespace Module\Content
 {
+    use Module\Content\Model\Entity\EntityPost;
+
     /**
-     * Factory With Valuable Parameter
+     * Build Array Response From Given Entity Object
      *
-     * @param mixed $contentName
-     * @param null  $contentData
+     * @param EntityPost $post
      *
-     * @return mixed
-     * @throws \Exception
+     * @return array
      */
-    static function of($contentName, $contentData = null)
+    function toArrayResponseFromPostEntity(EntityPost $post)
     {
-        if (!ContentIOC::ContentObjectContainer()->has($contentName))
-            throw new \Exception(sprintf('Content (%s) not registered as plugin.', $contentName));
-
-
-        $contentObject = ContentIOC::ContentObjectContainer()->get($contentName);
-        if ($contentData !== null) {
-            $contentObject->with($contentObject::parseWith($contentData));
-            if (!$contentObject->isFulfilled())
-                throw new \InvalidArgumentException(sprintf(
-                    'Content With Type (%s) not Fulfilled with given content.'
-                    , $contentName
-                ), 400);
-        }
-
-        return $contentObject;
+        return [
+            '$post' => [
+                'uid'        => (string) $post->getUid(),
+                'content'    => $post->getContent(),
+                'stat'       => $post->getStat(),
+                'stat_share' => $post->getStatShare(),
+                'location'   => [
+                    'caption' => $post->getLocation()->getCaption(),
+                    'geo'     => [
+                        'lon' => $post->getLocation()->getGeo('lon'),
+                        'lat' => $post->getLocation()->getGeo('lat'),
+                    ],
+                ],
+                'datetime_created' => [
+                    '$datetime' => $post->getDateTimeCreated(),
+                ],
+                'owner_identifier' => (string) $post->getOwnerIdentifier(),
+            ],
+        ];
     }
 }
 
+namespace Module\Content\Lib
+{
+    use Module\Content\Services\IOC as ContentIOC;
+    use Poirot\Std\Interfaces\Pact\ipFactory;
+
+    class FactoryContentObject
+        implements ipFactory
+    {
+        /**
+         * Factory With Valuable Parameter
+         *
+         * @param mixed $contentName
+         * @param null  $contentData
+         *
+         * @return mixed
+         * @throws \Exception
+         */
+        static function of($contentName, $contentData = null)
+        {
+            if (!ContentIOC::ContentObjectContainer()->has($contentName))
+                throw new \Exception(sprintf('Content (%s) not registered as plugin.', $contentName));
+
+
+            $contentObject = ContentIOC::ContentObjectContainer()->get($contentName);
+            if ($contentData !== null) {
+                $contentObject->with($contentObject::parseWith($contentData));
+                if (!$contentObject->isFulfilled())
+                    throw new \InvalidArgumentException(sprintf(
+                        'Content With Type (%s) not Fulfilled with given content.'
+                        , $contentName
+                    ), 400);
+            }
+
+            return $contentObject;
+        }
+    }
+}
